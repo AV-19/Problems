3333329a511192659ccf73121b16daa4e1a7c391
ANIKET VERMA <73159325+AV-19@users.noreply.github.com>
2023-02-08 17:25:38 +0530

    Create TourOfKing.java

---
+++ TourOfKing.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new  Scanner(System.in);
+		int t=sc.nextInt();
+		
+		while(t-->0){
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    
+		    int sum=x*5+y*7;
+		    System.out.println(sum);
+		}
+	}
+}
