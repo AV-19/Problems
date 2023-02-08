f7422ddcca65aa7e1eb4ff4d6110e660d864b05a
ANIKET VERMA <73159325+AV-19@users.noreply.github.com>
2023-02-08 17:39:38 +0530

    Create FindRating.java

---
+++ FindRating.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int x=n-a;
+		System.out.print(x+" ");
+		System.out.println(x-b);
+		
+			}
+}
