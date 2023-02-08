9db642e4b9c10199c67907faacfcc737c34b6652
ANIKET VERMA <73159325+AV-19@users.noreply.github.com>
2023-02-08 16:51:15 +0530

    Create KitchenTime.java

---
+++ KitchenTime.java
@@ -0,0 +1,15 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		 while(T-->0){
+		     int X=sc.nextInt();
+		     int Y=sc.nextInt();
+		      
+		      System.out.println(Y-X);
+		 }
+	}
+}
