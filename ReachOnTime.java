79d75beca9497e35ff6285372f7e957f7b2903e9
ANIKET VERMA <73159325+AV-19@users.noreply.github.com>
2023-02-08 17:07:55 +0530

    Create ReachOnTime.java

---
+++ ReachOnTime.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		
+		while(T-->0){
+		    try{
+		        int X=sc.nextInt();
+		        if(X>=30){
+		            System.out.println("YES");
+		        }else{
+		            System.out.println("NO");
+		        }
+		    }catch(Exception e){
+		        System.out.println(e);
+		    }
+		}
+	}
+}
