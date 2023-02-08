652edcbdbe4fa84413406a8e31b13da20bd56638
ANIKET VERMA <73159325+AV-19@users.noreply.github.com>
2023-02-08 19:11:56 +0530

    Create SavingTaxes.java

---
+++ SavingTaxes.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes 
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		
+		while(t-->0){
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    System.out.println(x-y);
+		}
+	}
+}
