0caf97c692c6c5915b4180da10c12029491917eb
ANIKET VERMA <73159325+AV-19@users.noreply.github.com>
2023-02-09 15:17:49 +0530

    Create GetSubscription.java

---
+++ GetSubscription.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		
+		while(t-->0){
+		    int x=sc.nextInt();
+		    if(x>30){
+		        System.out.println("YES");
+		    }else{
+		        System.out.println("NO");
+		    }
+		}
+	}
+}
