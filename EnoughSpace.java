81d07250761be36480653a6920bfbe7a4380d91a
ANIKET VERMA <73159325+AV-19@users.noreply.github.com>
2023-02-09 15:24:04 +0530

    Create EnoughSpace.java

---
+++ EnoughSpace.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+	    
+	    while(t-->0){
+	        int n=sc.nextInt();
+	        int x=sc.nextInt();
+	        int y=sc.nextInt();
+	        
+	        if(x+y*2<=n){
+	            System.out.println("YES");
+	        }else{
+	            System.out.println("NO");
+	        }
+	    }
+	}
+}
